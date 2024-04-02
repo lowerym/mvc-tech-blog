b375ccc2e9a4f27b27daab9d98a7d17ddeafe047
Matthew Lowery <matthewdlowery@msn.com>
2024-04-01 22:11:52 -0500

    Edited script file to include new comments

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,7 @@ var existingBlogs = document.querySelector("#existingBlogs");
 var createNew = document.querySelector("#createNew");
 var newPost = document.querySelector("#newPost");
 var newBlog = document.querySelector("#newBlog");
+var newComment = document.querySelector("#newComment");
 
 function hideCreateNew() {
   createNew.hidden=true;
@@ -10,8 +11,8 @@ function hideCreateNew() {
 hideCreateNew();
 
 newPost.addEventListener("submit", event => {
-  event.preventDefault()
-  console.log('click')
+  event.preventDefault();
+  console.log('click');
   existingBlogs.hidden=true;
   newPost.hidden=true;
   createNew.hidden=false;
@@ -20,10 +21,10 @@ newPost.addEventListener("submit", event => {
 newBlog.addEventListener("submit", event => {
   var title = document.querySelector("#title").value;
   var content = document.querySelector("#content").value;
-  event.preventDefault()
-  console.log('click')
+  event.preventDefault();
+  console.log('click');
   if (!title || !content) {
-    alert('Please enter both the title and content!')
+    alert('Please enter both the title and content!');
     return;
   }
   const blogObj = {
@@ -35,6 +36,35 @@ newBlog.addEventListener("submit", event => {
     body:JSON.stringify(blogObj),
     headers: {
       'Content-Type': 'application/json'
+    }
+  }).then(res => {
+    if(res.ok){
+      createNew.setAttribute('hidden', 'false');
+      location.reload();
+    } else {
+      alert('Error - Please Try Again');
+    }
+  })
+})
+
+newComment.addEventListener("submit", event => {
+  event.preventDefault();
+  const comment = {
+    body:document.querySelector("#comment").value,
+    blodID:document.querySelector("#hiddenCommentID").value,
+  }
+  fetch('/api/comments', {
+    method:'POST',
+    body:JSON.stringify(comment),
+    headers: {
+      'Content-Type':'application/json'
+    }
+  }).then(res => {
+    if(res.ok) {
+      console.log('Your Comment has been Posted!');
+      location.reload();
+    } else {
+      alert('Please Try Again!');
     }
   })
 })
