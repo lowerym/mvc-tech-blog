5732efd2a083f59bb90e5b8b77f7473d4bbcf59c
Matthew Lowery <matthewdlowery@msn.com>
2024-04-01 22:33:25 -0500

    Finished building out Javascript file

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,10 @@ var createNew = document.querySelector("#createNew");
 var newPost = document.querySelector("#newPost");
 var newBlog = document.querySelector("#newBlog");
 var newComment = document.querySelector("#newComment");
+var login = document.querySelector("#login");
+var signup = document.querySelector("#signup");
+var update = document.querySelector("#update");
+var remove = document.querySelector("#delete");
 
 function hideCreateNew() {
   createNew.hidden=true;
@@ -66,5 +70,92 @@ newComment.addEventListener("submit", event => {
     } else {
       alert('Please Try Again!');
     }
+  })
+})
+
+login.addEventListener("submit", event => {
+  event.preventDefault();
+  const userObj = {
+    username:document.querySelector('#loginUsername').value,
+    password:document.querySelector('#loginPassword').value,
+  }
+  console.log(userObj);
+  fetch('/api/users/login', {
+    method: 'POST',
+    body: JSON.stringify(userObj),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(res => {
+    if(res.ok) {
+      console.log('You have logged in successfully!');
+      location.href='/dashboard';
+    } else {
+      alert('Please Try Again!');
+    }
+  })
+})
+
+
+signup.addEventListener("submit",event => {
+  event.preventDefault();
+  const userObj = {
+      username:document.querySelector('#signupUsername').value,
+      password:document.querySelector('#signupPassword').value,
+  }
+  console.log(userObj)
+  fetch('/api/users/',{
+      method: 'POST',
+      body: JSON.stringify(userObj),
+      headers:{
+          'Content-Type': 'application/json'
+      }
+  }).then(res=>{
+      if(res.ok) {
+          console.log('You have signed in successfully!');
+          location.href= '/dashboard';
+      } else {
+          alert('Please Try Again!');
+      }
+  })
+})
+
+update.addEventListener("click",event=>{
+  event.preventDefault();
+  const blogID = document.querySelector("#hiddenBlogID").value;
+  const editBlog = {
+      title:document.querySelector("#editedTitle").value,
+      content:document.querySelector("#editedContent").value,
+  }
+  console.log(blogID);
+  console.log(editBlog);
+  fetch((`/api/blogs/${blogID}`),{
+      method: 'PUT',
+      body:JSON.stringify(editBlog),
+      headers:{
+          'Content-Type': 'application/json'
+      }
+  }).then(res=>{
+      if(res.ok){
+          console.log('The Blog Has Been Updated!')
+          location.href= '/dashboard';
+      } else {
+          alert('Please Try Again');
+      }
+  })
+})
+
+remove.addEventListener("click",event=>{
+  event.preventDefault();
+  const blogID = document.querySelector("#hiddenBlogID").value;
+  fetch((`/api/blogs/${blogID}`),{
+      method: 'DELETE',
+  }).then(res=>{
+      if(res.ok){
+          console.log('The Blog Has Been Deleted!');
+          location.href= '/dashboard';
+      } else {
+          alert('Please Try Again');
+      }
   })
 })
